53e4d061aa0c477526e8e592aa72f1091ef63759
Pavel Koryakin <unclekoin@gmail.com>
2021-08-23 11:16:55 +0300

    init: init project

---
+++ src/components/users.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import api from '../api';
+
+const Users = () => {
+  api.users.fetchAll();
+
+  return <h1>Users</h1>;
+}
+ 
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Users from './components/users';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Users />
   </React.StrictMode>,
   document.getElementById('root')
 );
