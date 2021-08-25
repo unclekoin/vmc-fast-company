57ed622086a9650f2225404051130e8195b42d6f
Pavel Koryakin <unclekoin@gmail.com>
2021-08-25 11:06:11 +0300

    feat: refactor code

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,19 +5,16 @@ const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
 
   const handleDelete = (userId) => {
-    setUsers((state) => {
-      return state.filter(({ _id }) => _id !== userId);
-    });
+    setUsers((state) => state.filter(({ _id }) => _id !== userId));
   };
 
   const renderPhrase = (number) => {
     if (!number) return 'Никто с тобой не тусанет';
 
-    const tail = +String(number).slice(-1);
-    const lastCouple = +String(number).slice(-2);
+    const tail = Number(String(number).slice(-1));
 
     const word =
-      (lastCouple >= 10 && lastCouple < 20) || tail < 2 || tail > 4
+      (number > 4 && number < 15) || ![2, 3, 4].includes(tail)
         ? 'человек'
         : 'человека';
 
@@ -43,38 +40,35 @@ const Users = () => {
 
   const rows = users.map(
     ({ _id, name, profession, qualities, completedMeetings, rate }) => (
-        <tr key={_id}>
-          <td>{name}</td>
-          <td>
-            {qualities.map(({ _id, name, color }) => (
-              <span key={_id} className={`badge bg-${color} me-2`}>
-                {name}
-              </span>
-            ))}
-          </td>
-          <td>{profession.name}</td>
-          <td>{completedMeetings}</td>
-          <td>{rate}/5</td>
-          <td>
-            <button
-              className="btn btn-danger"
-              onClick={() => handleDelete(_id)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      )
+      <tr key={_id}>
+        <td>{name}</td>
+        <td>
+          {qualities.map(({ _id, name, color }) => (
+            <span key={_id} className={`badge bg-${color} me-1`}>
+              {name}
+            </span>
+          ))}
+        </td>
+        <td>{profession.name}</td>
+        <td>{completedMeetings}</td>
+        <td>{rate}/5</td>
+        <td>
+          <button className="btn btn-danger" onClick={() => handleDelete(_id)}>
+            Удалить
+          </button>
+        </td>
+      </tr>
+    )
   );
 
   return (
     <div className="container mt-5">
       <h1 className="mb-4">
-        <span className={`badge ${users.length ? 'bg-primary' : 'bg-danger'}`}>
+        <span className={`badge bg-${users.length ? 'primary' : 'danger'}`}>
           {renderPhrase(users.length)}
         </span>
       </h1>
-      {(users.length || null) && (
+      {!!users.length && (
         <table className="table">
           <thead>
             <tr>{tableHead}</tr>
